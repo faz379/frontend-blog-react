1d5203e668a178f5028ce7ce5aee8e75c4d60da2
faz379 <fazelianpramudia@gmail.com>
2025-11-17 21:20:20 +0700

    icon show login

--- src/components/NotificationModal.jsx
+++ src/components/NotificationModal.jsx
@@ -7,11 +7,11 @@ export default function NotificationModal({ isOpen, title, message, onClose, typ
     <div className="fixed inset-0 bg-black bg-opacity-40 flex items-center justify-center z-50">
       <div
         className={`bg-white p-6 rounded-xl w-80 shadow-xl border 
-        ${type === "success" ? "border-green-500" : "border-red-500"}`}
+        ${type === "success" ? "border-orange-500" : "border-black"}`}
       >
         <h2
           className={`text-xl font-semibold mb-2 
-          ${type === "success" ? "text-green-600" : "text-red-600"}`}
+          ${type === "success" ? "text-orange-600" : "text-black"}`}
         >
           {title}
         </h2>
@@ -21,7 +21,7 @@ export default function NotificationModal({ isOpen, title, message, onClose, typ
         <button
           onClick={onClose}
           className={`w-full py-2 rounded-lg text-white font-semibold 
-          ${type === "success" ? "bg-green-600" : "bg-red-600"}`}
+          ${type === "success" ? "bg-orange-600" : "bg-black"} hover:opacity-90 transition`}
         >
           OK
         </button>
